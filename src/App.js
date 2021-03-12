ec65093da87457e8dccffcd44e074e60ad7a2f1f
Charlie-UWG <75844445+Charlie-UWG@users.noreply.github.com>
2021-03-12 19:56:36 +0900

    CsssModules, Styled Jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { CssModules } from "./components/CssModules";
 import { InlineStlyle } from "./components/InlineStyle";
+import { StyledJsx } from "./components/StyledJsx";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       <InlineStlyle />
+      <CssModules />
+      <StyledJsx />
     </div>
   );
 }
